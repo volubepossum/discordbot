7257dba34e3b4848fdfe175d17695c0f86452a23
volubepossum <63779780+volubepossum@users.noreply.github.com>
2020-04-16 21:57:34 +0200

    finished botbeta
    added 2 easter egg func, and cleanup, admin, anonim

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Heading = require("discord.js");
+
 const Connect = new Heading.Client();
 const Token = "NzAwMDU2MTI0NDk3MzMwMjQ3.XpdY6g.k6AvII9QQABaPloy_uta5Yu0AHA";
 const Prefix = "!"
@@ -11,74 +12,104 @@ Connect.on("ready",() => {console.log("Üdvözletem nagyuram!");});
                                                                 és akkor elkezdődik a nagy kaland
                                                                 ///////////////////////////////*/
 Connect.on("message", msg =>{
-    switch(msg.content){
-        case "Mi van?":
-            switch(new Date().getDay()){
-                case 0:
-                    msg.reply("Vasárnap");
-                    break;
-                case 1:
-                    msg.reply("Hétfő");
-                    break;
-                case 2:
-                    msg.reply("Kedd");
-                    break;
-                case 3:
-                    msg.reply("Szerda");
-                    break;
-                case 4:
-                    msg.reply("Csütörtök");
-                    break;
-                case 5:
-                    msg.reply("Péntek");
-                    break;
-                case 6:
-                    msg.reply("Szombat");
-                }
-             break;
-        case "mi van?":
-            switch(new Date().getDay()){
-                case 0:
-                    msg.reply("vasárnap");
-                    break;
-                case 1:
-                    msg.reply("hétfő");
-                    break;
-                case 2:
-                    msg.reply("kedd");
-                    break;
-                case 3:
-                    msg.reply("szerda");
-                    break;
-                case 4:
-                    msg.reply("csütörtök");
-                    break;
-                case 5:
-                    msg.reply("péntek");
-                    break;
-                case 6:
-                    msg.reply("szombat");
-                }
-             break;
-        case "Ki?":
-            msg.reply("Te!");
-            break;
-        case "ki":
-            msg.reply("te");
-            break;
-        case "ki?":
-            msg.reply("te!");
-            break;
+    //console.log(msg.content);
+    if (msg.author==="ClydeBot#1") { msg.delete(); }
+    if (msg.content.indexOf(Prefix)===0){
+        var arg = msg.content.substring(Prefix.length).split(" ");
+        var isSetPrefix = true;
+    }else {
+        var arg = msg.content.split(" ");
     }
-    let arg = msg.content.substring(Prefix.length).split(" ");
+    switch(msg.content){
+        default:
+            if (arg.indexOf("Mi") != -1 && arg.indexOf("van") != -1 && arg.indexOf("van")-arg.indexOf("Mi") === 1){
+                switch(new Date().getDay()){
+                    case 0:
+                        msg.reply("Vasárnap");
+                        break;
+                    case 1:
+                        msg.reply("Hétfő");
+                        break;
+                    case 2:
+                        msg.reply("Kedd");
+                        break;
+                    case 3:
+                        msg.reply("Szerda");
+                        break;
+                    case 4:
+                        msg.reply("Csütörtök");
+                        break;
+                    case 5:
+                        msg.reply("Péntek");
+                        break;
+                    case 6:
+                        msg.reply("Szombat");
+                    }
+            }else if (arg.indexOf("mi") != -1 && arg.indexOf("van") != -1 && arg.indexOf("van")-arg.indexOf("mi") === 1){
+                switch(new Date().getDay()){
+                    case 0:
+                        msg.reply("vasárnap");
+                        break;
+                    case 1:
+                        msg.reply("hétfő");
+                        break;
+                    case 2:
+                        msg.reply("kedd");
+                        break;
+                    case 3:
+                        msg.reply("szerda");
+                        break;
+                    case 4:
+                        msg.reply("csütörtök");
+                        break;
+                    case 5:
+                        msg.reply("péntek");
+                        break;
+                    case 6:
+                        msg.reply("szombat");
+                }
+            }else if (arg.indexOf("Ki") != -1) {
+                    msg.reply("Te!");
+            }else if (arg.indexOf("ki") != -1) {        
+                     msg.reply("te!");
+             }
+    }
     console.log(arg);
-    switch(arg[0]){
-        case "anonim":
-            msg.delete();
-            var anonim=[];
-            for (i = 1; i<arg.length; i++) {
-                anonim+=arg[i] + " ";
-            }
-            msg.channel.send(anonim);
+    if(isSetPrefix===true){
+        switch(arg[0]){
+            case "anonim":
+                msg.delete();
+                var message=[];
+                for (i = 1; i<arg.length; i++) {
+                    message+=arg[i] + " ";
+                }
+                msg.channel.send(message);
+                break;
+            case "admin":
+                msg.delete();
+                console.log(msg.member.roles.highest.name);
+                if (msg.member.roles.highest.name=="Király" || msg.member.roles.highest.name=="legfőbbb bizalmas") {
+                    msg.delete();
+                    var message=[];
+                    for (i = 1; i<arg.length; i++) {
+                        message+=arg[i] + " ";
+                    }
+                    message = "@here a Király szólt hozzátok, íme: " + message;
+                    msg.channel.send(message);
+                } else {
+                    msg.author.send("ehhez neked nincs jogod");
+                }
+            case "cleanup" :
+                msg.delete();
+                if (msg.member.roles.highest.name=="Király"){
+                    console.log(arg [1])
+                    msg.delete()
+                    var deletabi = msg.channel.messages.fetch({limit: parseInt(arg[1]) }).then(deletabi => msg.channel.bulkDelete(deletabi))
+                    console.log(deletabi);
+                    //msg.channel.bulkDelete(deletabi);
+                } else {
+                    msg.author.send("ehhez neked nincs jogod");
+                }
+        }
     }
 });
